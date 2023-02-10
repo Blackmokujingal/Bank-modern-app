0b9c4067f7165b5d93d9372db3546f21e2ea972d
Blackmokujingal <bea800.w98@gmail.com>
2023-02-10 14:53:30 +0400

    Added the structure & layout of the website

--- Bank-modern-app/src/App.jsx
+++ Bank-modern-app/src/App.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import styles from './style'
+import {Navbar,Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components';
 
-const App = () => {
-  return (
-    <div>
-      App
+
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={'${styles.paddingX} ${styles.flexCenter}'}>  
+      <div className={'${styles.boxWidth}'}>
+        <Navbar/>
+        </div>   
     </div>
-  )
-}
+
+    <div className={'bg-primary ${styles.flexStart}'}>
+      <div className={'${styles.boxWidht}'}>
+        Hero
+      </div>
+    </div>
+   
+
+    <div className={'bg-primary ${styles.flexStart} ${styles.paddingX}'}>
+      <div className={'${styles.boxWidht}'}>
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Testimonials/>
+        <Clients/>
+        <CTA/>
+        <Footer/>
+      </div>
+    </div>
+
+
+
+
+    </div>
+
+
+
+  );
+
 
 export default App
